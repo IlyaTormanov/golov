65426ef0a04561079cdb5f7f3ad3318840b6c651
vampiressa <yuliadervenkova@gmail.com>
2020-06-02 10:52:06 +0300

    mobile product

--- src/components/category/Product.tsx
+++ src/components/category/Product.tsx
@@ -5,7 +5,6 @@ import {useParams} from "react-router";
 import {galleryActions} from "../../redux/gallery/actions";
 import {batch, useDispatch, useSelector} from "react-redux";
 import {RootStateType} from "../../redux/root";
-import {shopInfoAction} from "../../redux/shopInfo/actions";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faTimes} from "@fortawesome/free-solid-svg-icons/faTimes";
 import {history} from "../../index";
@@ -22,11 +21,8 @@ import './product.css'
 import {faCheck} from "@fortawesome/free-solid-svg-icons/faCheck";
 import {findIndex, lookup} from "fp-ts/es6/Array";
 import {chain, isSome} from "fp-ts/es6/Option";
-import {productActions} from "../../redux/product/actions";
 import {faChevronLeft} from "@fortawesome/free-solid-svg-icons/faChevronLeft";
 import {faChevronRight} from "@fortawesome/free-solid-svg-icons/faChevronRight";
-import {api_v1} from "../../api";
-import {boolean} from "fp-ts";
 
 
 export interface ProductProps {
@@ -66,7 +62,7 @@ export type StorageItem = {
 }
 
 export interface Props {
-isLc?:boolean
+    isLc?: boolean
 }
 
 const useQuery = () => {
@@ -82,7 +78,7 @@ export const Product: FunctionComponent<Props> = (props) => {
     const dispatch = useDispatch();
     const preloader = useSelector((state: RootStateType) => state.gallery.productPreloader);
     const productList = useSelector((state: RootStateType) => state.gallery.gallery.res);
-    const userId=useSelector((state:RootStateType)=>state.auth.auth)
+    const userId = useSelector((state: RootStateType) => state.auth.auth)
     const {cust_id, product_id, gallery_id} = useParams<{ cust_id: string, product_id: string, gallery_id: string }>();
     const {isMobile, isDesktop} = useQuery();
 
@@ -102,18 +98,18 @@ export const Product: FunctionComponent<Props> = (props) => {
         JSON.parse(orderList)
     }
     const productData = useSelector((state: RootStateType) => state.gallery.galleryProduct);
-    const removeProduct=useCallback(()=>{
-        axios.delete('http://golowinskiy-api.bostil.ru/api/product',{
-            headers:{
-               Authorization:`Bearer ${userId.accessToken}`
+    const removeProduct = useCallback(() => {
+        axios.delete('http://golowinskiy-api.bostil.ru/api/product', {
+            headers: {
+                Authorization: `Bearer ${userId.accessToken}`
             },
-            data:{appCode:cust_id,cid:userId.userId,cust_ID:cust_id,prc_ID:productData.prc_ID.toString()}
-        },).then((res:any)=>{
-            if(res.data.result){
+            data: {appCode: cust_id, cid: userId.userId, cust_ID: cust_id, prc_ID: productData.prc_ID.toString()}
+        },).then((res: any) => {
+            if (res.data.result) {
                 history.goBack();
             }
         })
-    },[cust_id,userId,productData.prc_ID]);
+    }, [cust_id, userId, productData.prc_ID]);
 
 
     const [orderStatus, setOrderStatus] = useState(false);
@@ -158,32 +154,42 @@ export const Product: FunctionComponent<Props> = (props) => {
     }, [productList, product_id]);
     return (
         <div className={styles.detail_wrapper}>
-            <div className={`${styles.detail_product} ${productData.additionalImages?.length? '': styles.empty}`}>
-                <div className={styles.close_icon} onClick={() => history.push(props.isLc?`/${cust_id}/personalClient/${gallery_id}`:`/${cust_id}/${gallery_id}`)}>
-                    <FontAwesomeIcon icon={faTimes} color={'white'}
-                                     style={{width: '30px', height: '30px', color: '#95c6c3'}}/>
+            <div className={`${styles.detail_product} ${productData.additionalImages?.length ? '' : styles.empty}`}>
+                <div className={styles.close_icon}
+                     onClick={() => history.push(props.isLc ? `/${cust_id}/personalClient/${gallery_id}` : `/${cust_id}/${gallery_id}`)}>
+                    <FontAwesomeIcon
+                        icon={faTimes}
+                        color={'white'}
+                        style={{width: '30px', height: '30px', color: '#95c6c3'}}
+                    />
                 </div>
                 {(isSome(prev)) &&
-                <FontAwesomeIcon className={styles.left} onClick={() => history.push(prev.value)} icon={faChevronLeft}
-                                 style={{width: '30px', height: '30px',placeSelf:isMobile?'start':'center'}}/>
+                <FontAwesomeIcon
+                    className={styles.left}
+                    onClick={() => history.push(prev.value)} icon={faChevronLeft}
+                    style={{width: '30px', height: '30px'}}
+                />
                 }
                 {(isSome(next)) &&
-                <FontAwesomeIcon className={styles.right} onClick={() => history.push(next.value)} icon={faChevronRight}
-                                 style={{width: '30px', height: '30px' ,placeSelf:isMobile?'end':'center'}}/>
+                <FontAwesomeIcon
+                    className={styles.right}
+                    onClick={() => history.push(next.value)}
+                    icon={faChevronRight}
+                    style={{width: '30px', height: '30px'}}
+                />
                 }
                 <div className={styles.image}>
                     {preloader ?
-                        <ClipLoader size={36}
-                                    color={"white"}/>
+                        <ClipLoader
+                            size={36}
+                            color={"white"}
+                        />
                         :
-
                         <img
                             src={`http://golowinskiy-api.bostil.ru/api/Img?AppCode=${cust_id}&ImgFileName=${currentImg ? currentImg : productData.t_imageprev}`}/>
-
-
                     }
                 </div>
-                {(isMobile&&productData.additionalImages) &&
+                {(isMobile && productData.additionalImages) &&
                 <div className={styles.additional}>
                     {productData.additionalImages?.map(img =>
                         <img
@@ -203,27 +209,23 @@ export const Product: FunctionComponent<Props> = (props) => {
                             {productData.prc_Br}
                         </p>
                     </div>
-                    {props.isLc?
+                    {props.isLc ?
                         <div className={styles.button_grp}>
-                            <div style={{background:'#37c509'}}>
-                                <span>
-                                    Редактировать
-                                </span>
+                            <div style={{background: '#37c509'}}>
+                                <span>Редактировать</span>
                             </div>
-                            <div style={{background:'#f1173a'}} onClick={removeProduct}>
-                                <span>
-                                    Удалить
-                                </span>
+                            <div style={{background: '#f1173a'}} onClick={removeProduct}>
+                                <span>Удалить</span>
                             </div>
-                        </div>:
-                        <div className={styles.button_wrapper} style={{background: orderStatus ? '#37c509' : '#f1173a'}}>
-                            <div className={styles.to_cart} onClick={() => toOrder()}
+                        </div> :
+                        <div className={styles.button_wrapper}>
+                            <div
+                                className={styles.to_cart}
+                                onClick={() => toOrder()}
+                                style={{background: orderStatus ? '#37c509' : '#f1173a'}}
                             >
                                 {orderStatus ? <FontAwesomeIcon icon={faCheck} color={'white'}/> : <img src={cart}/>}
-                                <span>
-                                    {orderStatus ? 'В корзине' : 'В корзину'}
-
-                        </span>
+                                <span>{orderStatus ? 'В корзине' : 'В корзину'}</span>
                             </div>
                         </div>
                     }
